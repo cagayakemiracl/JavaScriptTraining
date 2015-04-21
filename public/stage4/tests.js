839b21853a7a5d8d06bf7d453d8297bd89e36043
cagayakemiracl <hirosnonno1182@gmail.com>
2015-04-21 23:17:57 +0900

    stage4 clear

--- public/stage4/tests.js
+++ public/stage4/tests.js
@@ -23,7 +23,10 @@ describe('ステージ4（意図通りにイベントを利用できる）', fun
       // });
       //
       // ここに上記のどちらかのコードを記述してください。
-
+      var element = document.querySelector('#firebrick');
+      element.onclick = function() {
+        element.textContent = Number(element.textContent) + 1;
+      };
 
       var firebrick = document.getElementById('firebrick');
       firebrick.dispatchEvent(createClickEvent());
@@ -37,6 +40,10 @@ describe('ステージ4（意図通りにイベントを利用できる）', fun
     it('2 番の要素の click イベントで要素内の数字を 1 ずつ小さくできる', function() {
 
       // ここにコードを記述してください。
+      var element = document.querySelector('#chocolate');
+      element.onclick = function() {
+        element.textContent = Number(element.textContent) - 1;
+      };
 
 
       var chocolate = document.getElementById('chocolate');
@@ -51,7 +58,12 @@ describe('ステージ4（意図通りにイベントを利用できる）', fun
     it('3 番の要素の click イベントで要素を 10 度ずつ回転できる', function() {
 
       // ここにコードを記述してください。
-
+      var element = document.querySelector('.mediumseagreen');
+      var angle = 0;
+      element.onclick = function() {
+        angle += 10;
+        element.style.transform = 'rotate(' + angle + 'deg)';
+      };
 
       var mediumseagreen = document.querySelector('.mediumseagreen');
       mediumseagreen.dispatchEvent(createClickEvent());
@@ -67,6 +79,12 @@ describe('ステージ4（意図通りにイベントを利用できる）', fun
     it('4 番の要素を入力された角度に回転できる', function() {
 
       // ここにコードを記述してください。
+      var element = document.querySelector('.turquoise');
+      var input = document.querySelector('input');
+      input.onchange = function() {
+        var angle = input.value;
+        element.style.transform = 'rotate(' + angle + 'deg)';
+      };
 
 
       var turquoise = document.querySelector('.turquoise');
@@ -93,8 +111,10 @@ describe('ステージ4（意図通りにイベントを利用できる）', fun
       // なお、expect(steelblue).to.be.null は上記のテストの要件を満たして
       // いないので、正解ではありません。
 
-      var steelblue = document.querySelector('.steelblue');
-      expect(steelblue).to.have.property('textContent', '5 \uD83D\uDC33');
+      $(window).ready(function() {
+        var steelblue = document.querySelector('.steelblue');
+        expect(steelblue).to.have.property('textContent', '5 \uD83D\uDC33');
+      });
       done();
     });
   });
